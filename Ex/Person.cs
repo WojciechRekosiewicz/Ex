fd00d5937d62c8aaa94779b15b24ca0d233b5b9e
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-05-10 12:40:53 +0200

    Add project files.

---
+++ Ex/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex
+{
+    class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+
+        public override bool Equals(object obj)
+        {
+            return true;
+        }
+
+
+        public override int GetHashCode()
+        {
+            return 13 * this.name.GetHashCode() * age.GetHashCode();
+        }
+
+
+      
+    }
+}
---
+++ Ex/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var people = new List<Person>();
+
+            var p1 = new Person("Adam", 30);
+            var p2 = new Person("Adam", 30);
+
+            if (!people.Contains(p1))
+                people.Add(p1);
+
+            if (!people.Contains(p2))
+                people.Add(p2);
+
+            Console.WriteLine(p1.GetHashCode());
+            Console.WriteLine(p2.GetHashCode());
+            Console.WriteLine(people.Count);
+        }
+    }
+}
